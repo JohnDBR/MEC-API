e4e70474d8b37fe309f2e9019af80632784e4859
John Barbosa <djbarbosa@uninorte.edu.co>
2019-08-21 10:12:31 -0500

    [chore]: production env has been updated
    - Using the Mec credentials in production for the emails
    
    Resolves: NA
    See also: NA

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
 	  :address              => "smtp.gmail.com",
 	  :port                 => 587,
-	  :user_name            => "imaginatuciuda@gmail.com", 
-	  :password             => "godmode01",
+	  :user_name            => "admin@mec.com.co", 
+	  :password             => "Mecltda123",
 	  :authentication       => 'plain',
 	  :enable_starttls_auto => true  }
 
